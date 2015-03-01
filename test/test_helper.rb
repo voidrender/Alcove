719dc2d5a946e4b3ffc66524b98689b08fd81497
Isaac Overacker <isaac@overacker.me>
2015-02-28 21:24:02 -0800

    Tweaking code climate token

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,3 @@
 require 'codeclimate-test-reporter'
-ENV['CODECLIMATE_REPO_TOKEN'] = '6298f1286a1bcb4263816164ce1a3ef5d6b8f7159f69b54c20c6fdc39ec3d173'
 CodeClimate::TestReporter.start
 puts "Started CodeClimate test reporter."
